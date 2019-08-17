5c71f0f4d2694eb08a195c625bad71f10a673b99
Mach Aurora <mach@aurora.git>
2019-08-17 21:49:56 +0800

    update menu-items to naviate withUpdate HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepages/homepage.component';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/homepages/homepage.component'
 
 function App() {
-  return (
-    <div>
-        <HomePage />
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/hats' component={HomePage}/>
+            </Switch>
+        </div>
+    )
 }
 
 export default App;
