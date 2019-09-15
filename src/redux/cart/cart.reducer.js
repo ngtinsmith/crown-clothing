446e56cfaf047109855a9e3f490abe65f975ceca
Mach Aurora <mach@aurora.git>
2019-09-15 18:21:17 +0800

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 const INITIAL_STATE = {
     hidden: true,
@@ -19,6 +19,26 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 // cartItems: [...state.cartItems, action.payload] // old no-quantiy
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    /*
+                        if (item ID is not equal to payload/to-be-removed item ID
+                            ~ passed from action)
+                            return it inside the cartItems array
+                        else
+                            filter it out (REMOVE IT)
+                    */
+                    cartItem => cartItem.id !== action.payload.id
+                )
             }
 
         default:
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -17,3 +17,29 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     // return cartItems with the NEW_ITEM
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(
+            /* 
+                if (item ID is not equal to cartItemToRemove.id)
+                    return it inside the cartItems array (INCLUDE IT)
+                else
+                    filter it out (REMOVE IT)
+            */
+            cartItem => cartItem.id !== cartItemToRemove.id
+        )
+    }
+
+    // if quantity > 1
+    return cartItems.map(
+        cartItem =>
+            cartItem.id === cartItemToRemove.id
+                ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+    )
+}
