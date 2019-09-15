abb32f56b45f92cb8f2c7c217ab6b9615b2c8406
Mach Aurora <mach@aurora.git>
2019-09-15 17:21:23 +0800

    added chechout page, checkout item component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { setCurrentUser } from './redux/user/user.action'
+import { createStructuredSelector } from 'reselect'
 
 import './App.css'
 
-import Header from './components/header/header.component'
 import HomePage from './pages/homepages/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
+
+import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.action'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 class App extends React.Component {
 
@@ -47,6 +51,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
+                    <Route exact path='/checkout' component={CheckoutPage} />
                     <Route exact path='/signin' render={
                         () => this.props.currentUser
                             ? (<Redirect to='/'/>)
@@ -58,8 +63,8 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
