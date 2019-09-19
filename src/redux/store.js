289f0380782b308211816444661f9639520ec8d2
Mach Aurora <mach@aurora.git>
2019-09-19 16:27:39 +0800

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,13 @@ import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
 
-const middewares = [logger]
+const middlewares = []
 
-export const store = createStore(
-    rootReducer,
-    applyMiddleware(...middewares)
-)
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export const persistor = persistStore(store)
 
