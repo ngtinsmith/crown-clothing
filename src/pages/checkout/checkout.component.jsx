f834f6a3ddd63549549bdfc1a19c3b5d4bdece27
Mach Aurora <mach@aurora.git>
2019-09-19 15:37:05 +0800

    updating stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,10 +3,14 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/srtipe-button/stripe-button.component'
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+import {
+    selectCartItems,
+    selectCartTotal
+} from '../../redux/cart/cart.selectors'
 
-import './check.styles.scss'
+import './checkout.styles.scss'
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -27,15 +31,18 @@ const CheckoutPage = ({ cartItems, total }) => (
                 <span>Remove</span>
             </div>
         </div>
-        {
-            cartItems.map(cartItem => (
-                    <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-                )
-            )
-        }
+        {cartItems.map(cartItem => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - expiry 01/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
