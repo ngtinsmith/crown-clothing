f7d1aeee6dde6c54c7c5dd69742a16c346b42875
Mach Aurora <mach@aurora.git>
2019-10-11 11:35:07 +0800

    optimized for mobile view

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
-import './App.css'
-
 import HomePage from './pages/homepages/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
 import CheckoutPage from './pages/checkout/checkout.component'
-
 import Header from './components/header/header.component'
+
+import { GlobalStyle } from './global.styles'
 
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { checkUserSession } from './redux/user/user.actions'
@@ -22,6 +21,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
     return (
         <div>
+            <GlobalStyle />
             <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
